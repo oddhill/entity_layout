0bdfaecac9e9f6583c2a7eedd08f6433c96c574b
Christoffer Palm <christoffer.palm@oddhill.se>
2016-01-22 10:51:26 +0100

    Deleted files that are no longer in use

--- src/Form/LayoutTypeDeleteForm.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\entity_layout\Form;
-
-use Drupal\Core\Entity\EntityDeleteForm;
-
-class LayoutTypeDeleteForm extends EntityDeleteForm {
-
-}
--- src/LayoutListBuilder.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\entity_layout;
-
-use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
-
-class LayoutListBuilder extends ConfigEntityListBuilder {
-
-}
